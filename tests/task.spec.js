896e15aac8a719435359c058d665cdeab318419a
AnbuSWTester <anbuart007@gmail.com>
2026-07-06 15:47:45 +0530

    test message

--- tests/task.spec.js
+++ tests/task.spec.js
@@ -2,6 +2,7 @@ import {test} from '@playwright/test'
 
 test('launch browser', async ({page}) => {
 
-    page.goto("https://www.flipkart.com/")
+    page.goto("https://www.flipkart.com/");
+    dfdfdcd
     
 });
