3752657bd4106a3defb8656d74e637ae39a9f205
AnbuSWTester <anbuart007@gmail.com>
2026-07-06 19:52:14 +0530

    message hi

---
+++ tests/task.spec.js
@@ -0,0 +1,10 @@
+import {test} from '@playwright/test'
+
+test('launch browser', async ({page}) => {
+
+    page.goto("https://www.flipkart.com/");
+    hdjdsfkjh
+    dfkhdhf
+    kdjfhsdhfkjf
+    
+});
